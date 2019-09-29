c27e04360d9c941b8ec1d40b02c7272b0494d2b1
sjscotton <sj.scotton@gmail.com>
2019-09-29 13:12:52 -0700

    Added heap_sort method

--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -2,7 +2,21 @@
 
 # This method uses a heap to sort an array.
 # Time Complexity:  ?
-# Space Complexity: ?
+# Space Complexity:
 def heap_sort(list)
-  raise NotImplementedError, "Method not implemented yet..."
+  heap = heapify(list)
+  until heap.empty?
+    item = heap.remove()
+    list.append(item)
+  end
+  return list
+end
+
+def heapify(list)
+  heap = MinHeap.new()
+  until list.empty?
+    item = list.pop()
+    heap.add(item)
+  end
+  return heap
 end
--- test/heapsort_test.rb
+++ test/heapsort_test.rb
@@ -1,36 +1,36 @@
 require_relative "test_helper"
 
-xdescribe "heapsort" do
+describe "heapsort" do
   it "sorts an empty array" do
-    # Arrange 
+    # Arrange
     list = []
 
     # Act
-    result = heapsort(list)
+    result = heap_sort(list)
 
     # Assert
     expect(result).must_equal []
   end
 
   it "can sort a 1-element array" do
-    # Arrange 
+    # Arrange
     list = [5]
 
     # Act
-    result = heapsort(list)
+    result = heap_sort(list)
 
     # Assert
     expect(result).must_equal [5]
   end
-  
+
   it "can sort a 5-element array" do
-    # Arrange 
+    # Arrange
     list = [5, 27, 3, 16, -50]
 
     # Act
-    result = heapsort(list)
+    result = heap_sort(list)
 
     # Assert
     expect(result).must_equal [-50, 3, 5, 16, 27]
-  end  
+  end
 end
